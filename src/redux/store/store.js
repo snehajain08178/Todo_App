9d4a3e01b4c3e1a66bd0557c2a59b1452ba9b311
Sneha Jain <62318433+snehajain08178@users.noreply.github.com>
2020-03-28 17:55:00 +0530

    Update store.js

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,10 +1,19 @@
 import {createStore, combineReducers} from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
 import addingReducer from './../reducer/reducer';
+import { AsyncStorage } from 'react-native';
 
 const rootReducer = combineReducers({
   mainReducer: addingReducer,
 });
 
-const configureStore = () => createStore(rootReducer);
+const persistConfig = {
+  key: 'root',
+  storage: AsyncStorage
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+const configureStore = () => createStore(persistedReducer);
 
 export default configureStore;
